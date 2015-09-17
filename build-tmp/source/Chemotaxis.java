fb2d03ced2e89d9292e2ce7af8a45edc94d7b387
unknown <thtran1@697-334-Boromir.sf.k12.internal>
2015-09-17 11:40:29 -0700

    9/17 update

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -16,20 +16,22 @@ public class Chemotaxis extends PApplet {
 
  Bacteria [] colony;
  Food a;
- int mX = 250;
- int mY = 250;
+ int sizeS = 1000; //size of canvas, use to modify: food initial position, bacteria initial position, bacteria spawning zone, black rectangle size[makes trails]
+ int mX = sizeS/2;
+ int mY = sizeS/2;
  public void setup()   
  {     
- 	size(500,500);
- 	colony = new Bacteria[500];
+ 	size(sizeS,sizeS);
+ 	colony = new Bacteria[1000];
  	for (int i = 0; i < colony.length; i++)
  	{
- 		colony[i] = new Bacteria(250,250);
+ 		colony[i] = new Bacteria(sizeS/2,sizeS/2);
  	}
  }   
  public void draw()   
  {    
- 	background(0);
+ 	fill(0,80);
+ 	rect(-100,-100,sizeS+100,sizeS+100);
  	for (int i = 0; i < colony.length; i++)
  	{
  		colony[i].move();
@@ -45,13 +47,14 @@ public class Chemotaxis extends PApplet {
  	{
  		xPos = x;
  		yPos = y;
- 		sizeB = (int)(Math.random()*6)+5;
+ 		sizeB = (int)(Math.random()*2)+10;
  		rCol = (int)(Math.random()*257);
  		gCol = (int)(Math.random()*257);
  		bCol = (int)(Math.random()*257);
  	}
  	public void move()
  	{
+ 		//Go to food
  		if (xPos < mX)
  		{
  			xPos = xPos + ((int)(Math.random()*2)-0); //0,1
@@ -75,9 +78,46 @@ public class Chemotaxis extends PApplet {
  		if (xPos == mX && yPos == mY)
  		{
  			int [] numbers = {-1,1};
- 			xPos = (int)(Math.random()*501)+500*numbers[(int)(Math.random()*2)];
- 			yPos = (int)(Math.random()*501)+500*numbers[(int)(Math.random()*2)];
+ 			xPos = (int)(Math.random()*sizeS)+sizeS*numbers[(int)(Math.random()*2)];
+ 			yPos = (int)(Math.random()*sizeS)+sizeS*numbers[(int)(Math.random()*2)];
  		}
+
+ 		//Move away from other bacteria ***NOT WORKING***
+ 		/*for (int i = 1; i <= 3; i++)
+ 		{
+	 		if (get(xPos+(sizeB/2)+i,yPos) != 0)
+	 		{
+	 			xPos = xPos + ((int)(Math.random()*2)-1); //-1,0
+	 		}
+	 		else if (get(xPos+(sizeB/2)+i,yPos) == 0)
+	 		{
+	 			xPos = xPos;
+	 		}
+	 		if (get(xPos-(sizeB/2)+i,yPos) != 0)
+	 		{
+	 			xPos = xPos + ((int)(Math.random()*2)-0); //0,1
+	 		}
+	 		else if (get(xPos-(sizeB/2)+i,yPos) == 0)
+	 		{
+	 			xPos = xPos;
+	 		}
+	 		if (get(xPos,yPos+(sizeB/2)+i) != 0)
+	 		{
+	 			yPos = yPos + ((int)(Math.random()*2)-1); //-1,0
+	 		}
+	 		else if (get(xPos,yPos+(sizeB/2)+i) == 0)
+	 		{
+	 			yPos = yPos;
+	 		}
+	 		if (get(xPos,yPos-(sizeB/2)+i) != 0)
+	 		{
+	 			yPos = yPos + ((int)(Math.random()*2)-0); //0,1
+	 		}
+	 		else if (get(xPos,yPos-(sizeB/2)+i) == 0)
+	 		{
+	 			yPos = yPos;
+	 		}
+	 	}*/
  	}
  	public void show()
  	{
@@ -99,13 +139,13 @@ public class Chemotaxis extends PApplet {
  	public void show()
  	{
  		stroke(255,255,255);
- 		text("(" + (xPos+sizeF/2)/10 + "," + (yPos+sizeF/2)/10 + ")",xPos+sizeF/2, yPos-sizeF/2);
+ 		text("(" + (int)(xPos+sizeF/2)/10 + "," + (int)(yPos+sizeF/2)/10 + ")",xPos+sizeF/2, yPos-sizeF/2);
  		noStroke();
  		fill(255,255,0);
  		rect(xPos+sizeF/2,yPos+sizeF/2,sizeF,sizeF);
  	}
  }    
- public void mousePressed()
+ public void mouseMoved()
  {
  	mX = mouseX;
  	mY = mouseY;
